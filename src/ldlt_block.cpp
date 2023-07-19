b52d2099c49f467b934586baa76219f9c8ce30ad
map9959 <map9959@nyu.edu>
2023-07-19 18:21:03 -0400

    inlined matmul, coarse parallelism is faster fine-grained is unmaintained. will fix later

--- src/ldlt_block.cpp
+++ src/ldlt_block.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <iostream>
 #include <omp.h>
+#include <oneapi/mkl/blas.hpp>
 
 /*
     This function stores and accesses FORTRAN arrays with column-first notation.
@@ -32,32 +33,37 @@ void ldlt_block(float *matrix, size_t n){
         ldlt_serial(&matrix[BLOCK_J*blocks*bi+BLOCK_J*bi]);
 
         //resolve in-block dependencies
+        #pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
         for(int bj = bi + 1; bj < blocks; bj++){
+            //for each row in each block
             for(int k = 0; k < BLOCK_SIZE; k++){
-                //store current column
-                memcpy(&aux[BLOCK_J*bj+BLOCK_SIZE*k], &matrix[BLOCK_J*blocks*bi+BLOCK_J*bj+BLOCK_SIZE*k], BLOCK_SIZE*sizeof(float));
-                //divide column by diagonal element
+                //for each element in each row
                 for(int l = 0; l < BLOCK_SIZE; l++){
-                    matrix[BLOCK_J*blocks*bi + BLOCK_J*bj + BLOCK_SIZE*k + l] /= matrix[BLOCK_J*blocks*bi + BLOCK_J*bi + BLOCK_SIZE*k + k];
-                }
-                //subtract from the right
-                for(int l = k+1; l < BLOCK_SIZE; l++){
-                    for(int m = 0; m < BLOCK_SIZE; m++){
-                        matrix[BLOCK_J*blocks*bi + BLOCK_J*bj + BLOCK_SIZE*m + l] -= aux[BLOCK_J*bj+BLOCK_SIZE*m+k] * matrix[BLOCK_J*blocks*bi+BLOCK_J*bi+BLOCK_SIZE*k+l];
+                    //store element
+                    aux[BLOCK_J*bj + BLOCK_SIZE*l + k] = matrix[BLOCK_J*blocks*bi + BLOCK_J*bj + BLOCK_SIZE*l + k];
+                    //divide by diagonal
+                    matrix[BLOCK_J*blocks*bi + BLOCK_J*bj + BLOCK_SIZE*l + k] /= matrix[BLOCK_J*blocks*bi + BLOCK_J*bi + BLOCK_SIZE*l + l];
+                    //subtract from the right
+                    for(int m = l+1; m < BLOCK_SIZE; m++){
+                        matrix[BLOCK_J*blocks*bi + BLOCK_J*bj + BLOCK_SIZE*m + k] -= aux[BLOCK_J*bj+BLOCK_SIZE*l+k] * matrix[BLOCK_J*blocks*bi+BLOCK_J*bi+BLOCK_SIZE*l+m];
                     }
                 }
             }
         }
         
         //right-looking section of the LDL^T algorithm
+        //#pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
         for(int bj = bi+1; bj < blocks; bj++){
             for(int k = bi+1; k <= bj; k++){
                 //matrix multiplication and subtraction, -LDL^T
+                #pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
                 for(int i = 0; i < BLOCK_SIZE; i++){
                     for(int j = 0; j < BLOCK_SIZE; j++){
+                        float temp = 0.f;
                         for(int m = 0; m < BLOCK_SIZE; m++){
-                            matrix[BLOCK_J*blocks*k + BLOCK_J*bj + BLOCK_SIZE*i + j] -= (aux[BLOCK_J*bj + BLOCK_SIZE*m + i] + matrix[BLOCK_J*blocks*bi + BLOCK_J*k + BLOCK_SIZE*m + j]);
+                            temp -= (aux[BLOCK_J*bj + BLOCK_SIZE*m + i] * matrix[BLOCK_J*blocks*bi + BLOCK_J*k + BLOCK_SIZE*m + j]);
                         }
+                        matrix[BLOCK_J*blocks*k + BLOCK_J*bj + BLOCK_SIZE*i + j] = temp;
                     }
                 }
             }
@@ -181,38 +187,41 @@ void ldlt_coarse(PackedSymmetricMatrix<float> &matrix, size_t n, queue &q){
         //resolve in-block dependencies
         q.submit([=](handler &h){
             for(int bj = bi+1; bj < blocks; bj++){
-                matrix.addBlockToNegative(&aux[BLOCK_J*bj], bi, bj, BLOCK_SIZE);;
+                matrix.addBlockToNegative(&workspace[BLOCK_J*blocks*bi + BLOCK_J*bj], bi, bj, BLOCK_SIZE);
             }
-        }).wait();
-        q.submit([=](handler &h){
             h.parallel_for(range<2>{blocks, BLOCK_SIZE}, [=](id<2> idx){
                 const int bj = idx[0];
                 const int k = idx[1];
                 if(bj > bi){
                     for(int l = 0; l < BLOCK_SIZE; l++){
-                        aux[BLOCK_J*bj + BLOCK_SIZE*k + l] -= workspace[BLOCK_J*blocks*bi + BLOCK_J*bj + BLOCK_SIZE*k + l];
-                        workspace[BLOCK_J*blocks*bi + BLOCK_J*bj + BLOCK_SIZE*k + l] = aux[BLOCK_J*bj + BLOCK_SIZE*k + l];
-                        workspace[BLOCK_J*blocks*bi + BLOCK_J*bj + BLOCK_SIZE*k + l] /= workspace[BLOCK_J*blocks*bi + BLOCK_J*bi + BLOCK_SIZE*k + k];
+                        aux[BLOCK_J*bj + BLOCK_SIZE*l + k] = workspace[BLOCK_J*blocks*bi + BLOCK_J*bj + BLOCK_SIZE*l + k];
+                        workspace[BLOCK_J*blocks*bi + BLOCK_J*bj + BLOCK_SIZE*l + k] /= workspace[BLOCK_J*blocks*bi + BLOCK_J*bi + BLOCK_SIZE*l + l];
                         for(int m = l+1; m < BLOCK_SIZE; m++){
-                            workspace[BLOCK_J*blocks*bi + BLOCK_J*bj + BLOCK_SIZE*m + l] += aux[BLOCK_J*bj+BLOCK_SIZE*k+l] * workspace[BLOCK_J*blocks*bi+BLOCK_J*bi+BLOCK_SIZE*l+m];
+                            workspace[BLOCK_J*blocks*bi + BLOCK_J*bj + BLOCK_SIZE*m + k] -= aux[BLOCK_J*bj+BLOCK_SIZE*l+k] * workspace[BLOCK_J*blocks*bi+BLOCK_J*bi+BLOCK_SIZE*l+m];
                         }
                     }
                 }
             });
-        }).wait();
-        
-        q.submit([=](handler &h){
             for(int bj = bi+1; bj < blocks; bj++){
                 matrix.changeBlock(&workspace[BLOCK_J*blocks*bi+BLOCK_J*bj], bi, bj, BLOCK_SIZE);
             }
-        });
-        q.wait();
+        }).wait();
 
         //right-looking section of the LDL^T algorithm
         for(int bj = bi+1; bj < blocks; bj++){
             for(int k = bi+1; k <= bj; k++){
-                //matrix multiplication and subtraction, -LDL^T
-                mm_kernel_abt(q, &aux[BLOCK_J*bj], &workspace[BLOCK_J*blocks*bi + BLOCK_J*k], &workspace[BLOCK_J*blocks*k + BLOCK_J*bj], -1, BLOCK_SIZE);
+                //matrix multiplication and addition, -LDL^T
+                q.submit([&](handler &h){
+                    h.parallel_for(range<2>{BLOCK_SIZE,BLOCK_SIZE}, [=](item<2> item){
+                        const int i = item.get_id(0);
+                        const int j = item.get_id(1);
+                        float temp = 0.f;
+                        for (int k = 0; k < BLOCK_SIZE; k++) {
+                            temp += aux[BLOCK_J*bj+k*BLOCK_SIZE+i] * workspace[BLOCK_J*blocks*bi + BLOCK_J*k+k*BLOCK_SIZE+j];
+                        }
+                        workspace[BLOCK_J*blocks*k + BLOCK_J*bj+j*BLOCK_SIZE+i] = temp;
+                    });
+                });
             }
         }
         q.wait();
@@ -241,7 +250,7 @@ void mm_kernel(queue &q, float *matrix_a, float *matrix_b, float *matrix_c, floa
     host_accessor hc(c, read_only);
 }
 
-//multiplies two column-order matrices A, B^T of size NxN and adds them to C
+//multiplies two column-order matrices A, B^T of size NxN and adds them to C. requires dependencies
 event mm_kernel_abt(queue &q, float *matrix_a, float *matrix_b, float *matrix_c, float alpha, size_t N) {
     buffer a(matrix_a, range<1>{N*N});
     buffer b(matrix_b, range<1>{N*N});
@@ -255,9 +264,52 @@ event mm_kernel_abt(queue &q, float *matrix_a, float *matrix_b, float *matrix_c,
         h.parallel_for(range<2>{N,N}, [=](item<2> item){
             const int i = item.get_id(0);
             const int j = item.get_id(1);
+            float temp = 0.f;
             for (int k = 0; k < N; k++) {
-                C[j*N+i] += alpha * A[k*N+i] * B[k*N+j];
+                temp += alpha * A[k*N+i] * B[k*N+j];
             }
+            C[j*N+i] = temp;
+        });
+    });
+    host_accessor hc(c, read_only);
+    return e;
+}
+
+//multiplies two column-order matrices A, B^T of size NxN and adds them to C
+event mm_kernel_abt_local(queue &q, float *matrix_a, float *matrix_b, float *matrix_c, float alpha, size_t N, size_t M) {
+    buffer a(matrix_a, range<1>{N*N});
+    buffer b(matrix_b, range<1>{N*N});
+    buffer c(matrix_c, range<1>{N*N});
+
+    auto e = q.submit([&](handler &h){
+        accessor A(a, h, read_only);
+        accessor B(b, h, read_only);
+        accessor C(c, h, write_only);
+
+        range<2> global_size(M,M);
+        range<2> work_group_size(M,M);
+
+        local_accessor<float, 2> A_tile(range<2>(M, M), h);
+        local_accessor<float, 2> B_tile(range<2>(M, M), h);
+
+        h.parallel_for(nd_range<2>{global_size, work_group_size}, [=](nd_item<2> item){
+            const int i = item.get_global_id(0);
+            const int j = item.get_global_id(1);
+            const int x = item.get_local_id(0);
+            const int y = item.get_local_id(1);
+
+            float temp = 0.f;
+            int k;
+            for (int t = 0; t < N; t += M) {
+                A_tile[x][y] = A[i * N + (t + y)];
+                B_tile[x][y] = B[(t + x) * N + j];
+                item.barrier(access::fence_space::local_space);
+                for (k = 0; k < M; k++) {
+                    temp += alpha * A_tile[k][x] * B_tile[k][y];
+                }
+            }
+            C[j*N+i] = temp;
+            
         });
     });
     host_accessor hc(c, read_only);
